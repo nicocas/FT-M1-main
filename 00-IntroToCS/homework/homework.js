5af8511fb6fcca2f56d8b361e589f2615ffd3b2b
nicocas <nicocastelli94@gmail.com>
2022-04-12 12:03:59 -0300

    homework cs

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,12 +2,22 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+ var numero=0;
+for (let i=0 ; i<num.length ; i++){
+numero= numero + num[i] *2 ** (num.length-1-i);
 }
+return numero;
+}
+
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  var resultado= '';
+while (num/2>0){
+  resultado=num%2+resultado;
+  num=Math.floor(num/2)
+}
+return resultado;
 }
 
 
